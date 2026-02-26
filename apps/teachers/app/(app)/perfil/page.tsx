9f5e76f5a1d4de62872692f45561791667489072
Fernanda Felix <fernandaleitefelix@gmail.com>
2026-02-26 16:02:50 -0300

    fix: remove comments

--- apps/teachers/app/(app)/perfil/page.tsx
+++ apps/teachers/app/(app)/perfil/page.tsx
@@ -2,7 +2,6 @@ import { getProfileData } from "@/services/profile";
 import { ProfileClientContent } from "./_components/page-client";
 import { PageHeader } from "@repo/ui/components/page-header";
 
-
 export default async function EditProfilePage() {
   const data = await getProfileData();
 
@@ -15,12 +14,6 @@ export default async function EditProfilePage() {
           title='Meu perfil'
           subtitle='Acompanhe seu progresso e gerencie suas informações.'
         />
-        {/* <h2 className="text-3xl font-extrabold tracking-tight mb-2">
-          Meu perfil
-        </h2>
-        <p className="text-[#8B8265]">
-          Acompanhe seu progresso e gerencie suas informações.
-        </p> */}
       </div>
 
       <ProfileClientContent user={data.user} />
