35071d23157b1d0a6d3ccbca508d53c96ae94616
Fernanda Felix <fernandaleitefelix@gmail.com>
2026-04-30 17:23:40 -0300

    fix ports

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
+      allowedOrigins: [
+        "localhost:3000",
+        "j7flrdqb-3000.brs.devtunnels.ms",
+        "localhost:3001",
+        "j7flrdqb-3001.brs.devtunnels.ms",
+        "localhost:3002",
+        "j7flrdqb-3002.brs.devtunnels.ms",
+      ],
     },
   },
   images: {
--- apps/students/next.config.ts
+++ apps/students/next.config.ts
@@ -7,8 +7,15 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      bodySizeLimit: "5mb",
-      allowedOrigins: ["localhost:3000", "j7flrdqb-3000.brs.devtunnels.ms"],
+      bodySizeLimit: "15mb",
+      allowedOrigins: [
+        "localhost:3000",
+        "j7flrdqb-3000.brs.devtunnels.ms",
+        "localhost:3001",
+        "j7flrdqb-3001.brs.devtunnels.ms",
+        "localhost:3002",
+        "j7flrdqb-3002.brs.devtunnels.ms",
+      ],
     },
   },
   images: {
