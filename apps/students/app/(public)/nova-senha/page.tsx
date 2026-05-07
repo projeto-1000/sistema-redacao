d2360dd34d040235bd653b32282b46186e104c46
Fernanda Felix <fernandaleitefelix@gmail.com>
2026-05-07 20:59:23 -0300

    fix(logo): fix logo in new password page

--- apps/students/app/(public)/nova-senha/page.tsx
+++ apps/students/app/(public)/nova-senha/page.tsx
@@ -1,15 +1,16 @@
 import { SetPasswordForm } from "@/components/set-password-form";
+import { Shield, ShieldCheck } from "lucide-react";
 
 
 export default function NovaSenhaPage() {
   return (
-    <div className="bg-slate-100 min-h-dvh flex items-center flex-col justify-center p-4">
+    <div className="bg-gradient-soft min-h-dvh flex items-center flex-col justify-center p-4">
 
 
       <SetPasswordForm />
 
-      <footer className="mt-8 text-center text-sm text-zinc-500">
-        Ambiente seguro e criptografado.
+      <footer className="mt-8 text-center text-[12px] text-slate-500 flex items-center gap-2 font-bold uppercase tracking-wider">
+        <ShieldCheck className="text-emerald-500 size-4" />Ambiente seguro e criptografado.
       </footer>
     </div>
   )
--- apps/students/components/set-password-form.tsx
+++ apps/students/components/set-password-form.tsx
@@ -7,6 +7,7 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@repo
 import { Checkbox } from '@repo/ui/components/checkbox'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@repo/ui/components/form'
 import { Input } from '@repo/ui/components/input'
+import { Logo } from '@repo/ui/components/logo'
 import { getErrorMessage } from '@repo/utils'
 import { type SetPasswordSchema, setPasswordSchema } from '@repo/validators'
 import { EyeOff, Eye } from 'lucide-react'
@@ -50,119 +51,124 @@ export function SetPasswordForm() {
   };
 
   return (
-    <Card className="w-full max-w-[500px] bg-white rounded-xl shadow-xl border border-slate-100 px-5 py-6 md:py-8 md:px-6">
-      <CardHeader className="text-center gap-2">
-        <CardTitle className="text-2xl font-bold leading-tight">Defina sua senha</CardTitle>
-        <CardDescription className="text-slate-500 text-sm sm:text-base md:text-[16px]">
-          Crie uma senha segura para acessar a área de aluno.
-        </CardDescription>
-      </CardHeader>
+    <div className="w-full max-w-[500px] flex flex-col items-center">
 
-      <CardContent className="p-0">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-slate-700 uppercase tracking-wider text-[13px]">Senha</FormLabel>
-                  <FormControl>
-                    <div className="relative">
-                      <Input
-                        className="w-full rounded-2xl h-12 p-3.5 focus-visible:ring-primary focus-visible:border-primary focus-visible:ring-1"
-                        type={showPassword ? "text" : "password"}
-                        placeholder="Crie uma senha segura"
-                        {...field}
-                      />
-                      <Button
-                        type="button"
-                        variant="ghost"
-                        size="icon"
-                        className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                        onClick={() => setShowPassword(!showPassword)}
-                      >
-                        {showPassword ? (
-                          <EyeOff className="h-4 w-4 text-muted-foreground" />
-                        ) : (
-                          <Eye className="h-4 w-4 text-muted-foreground" />
-                        )}
-                      </Button>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+      <Logo className="h-20 md:h-24 mb-8 mt-4" />
 
+      <Card className="w-full max-w-[500px] bg-white rounded-xl shadow-xl border border-slate-100 px-5 py-6 md:py-8 md:px-6">
+        <CardHeader className="text-center gap-2">
+          <CardTitle className="text-2xl font-bold leading-tight">Defina sua senha</CardTitle>
+          <CardDescription className="text-slate-500 text-sm sm:text-base md:text-[16px]">
+            Crie uma senha segura para acessar a área de aluno.
+          </CardDescription>
+        </CardHeader>
 
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-slate-700 uppercase tracking-wider text-[13px]">Confirmar Senha</FormLabel>
-                  <FormControl>
-                    <div className="relative">
-                      <Input
-                        className="w-full rounded-2xl h-12 p-3.5 focus-visible:ring-primary focus-visible:border-primary focus-visible:ring-1"
-                        type={showConfirmPassword ? "text" : "password"}
-                        placeholder="Confirme sua senha"
-                        {...field}
-                      />
-                      <Button
-                        type="button"
-                        variant="ghost"
-                        size="icon"
-                        className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                      >
-                        {showConfirmPassword ? (
-                          <EyeOff className="h-4 w-4 text-muted-foreground" />
-                        ) : (
-                          <Eye className="h-4 w-4 text-muted-foreground" />
-                        )}
-                      </Button>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="terms"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start my-6">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                      className="mt-0.5 data-[state=checked]:bg-primary data-[state=checked]:border-primary"
-                    />
-                  </FormControl>
-                  <div className="space-y-2 leading-none">
-                    <FormLabel className="text-slate-600 font-medium leading-relaxed">
-                      Eu concordo com os Termos de Uso e Políticas de Privacidade da plataforma.
-                    </FormLabel>
+        <CardContent className="p-0">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-slate-700 uppercase tracking-wider text-[13px]">Senha</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          className="w-full rounded-2xl h-12 p-3.5 focus-visible:ring-primary focus-visible:border-primary focus-visible:ring-1"
+                          type={showPassword ? "text" : "password"}
+                          placeholder="Crie uma senha segura"
+                          {...field}
+                        />
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="icon"
+                          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? (
+                            <EyeOff className="h-4 w-4 text-muted-foreground" />
+                          ) : (
+                            <Eye className="h-4 w-4 text-muted-foreground" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
                     <FormMessage />
-                  </div>
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="w-full font-medium h-12 rounded-2xl text-[16px]"
-              disabled={isSubmitting || !isValid}
-              isLoading={isSubmitting}
-              loadingText='Salvando...'
-            >
-              Salvar e Acessar Plataforma
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+                  </FormItem>
+                )}
+              />
+
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-slate-700 uppercase tracking-wider text-[13px]">Confirmar Senha</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          className="w-full rounded-2xl h-12 p-3.5 focus-visible:ring-primary focus-visible:border-primary focus-visible:ring-1"
+                          type={showConfirmPassword ? "text" : "password"}
+                          placeholder="Confirme sua senha"
+                          {...field}
+                        />
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="icon"
+                          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                          onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                        >
+                          {showConfirmPassword ? (
+                            <EyeOff className="h-4 w-4 text-muted-foreground" />
+                          ) : (
+                            <Eye className="h-4 w-4 text-muted-foreground" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="terms"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start my-6">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        className="mt-0.5 data-[state=checked]:bg-primary data-[state=checked]:border-primary"
+                      />
+                    </FormControl>
+                    <div className="space-y-2 leading-none">
+                      <FormLabel className="text-slate-600 font-medium leading-relaxed">
+                        Eu concordo com os Termos de Uso e Políticas de Privacidade da plataforma.
+                      </FormLabel>
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full font-medium h-12 rounded-2xl text-[16px]"
+                disabled={isSubmitting || !isValid}
+                isLoading={isSubmitting}
+                loadingText='Salvando...'
+              >
+                Salvar e Acessar Plataforma
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
