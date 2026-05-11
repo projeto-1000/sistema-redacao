b0f39ca13f61e2fb7a8b094d425f31ba9da47750
Fernanda Felix <fernandaleitefelix@gmail.com>
2026-05-11 16:08:29 -0300

    fix: teacher payment button

--- apps/admin/components/teacher-profile-header.tsx
+++ apps/admin/components/teacher-profile-header.tsx
@@ -5,6 +5,7 @@ import { useToggleUserStatus } from "@/hooks/use-toggle-user-status";
 import { TeacherProfile } from "@repo/types";
 import { CreditCard, ChevronRight } from "lucide-react";
 import { UserProfileHeader } from "./user-profile-header";
+import Link from "next/link";
 interface TeacherProfileHeaderProps {
   teacher: TeacherProfile
 }
@@ -21,13 +22,25 @@ export default function TeacherProfileHeader({ teacher }: TeacherProfileHeaderPr
       onToggleStatus={toggleStatus}
       footer={
         <div className=" bg-slate-100 p-4 px-8">
-          <button className="flex items-center justify-between w-full text-sm font-bold hover:text-blue-600 transition-colors group">
+          {/* <button className="flex items-center justify-between w-full text-sm font-bold hover:text-blue-600 transition-colors group" href='/pagamentos'>
             <span className="flex items-center gap-2">
               <CreditCard className="size-4.5 text-slate-400 group-hover:text-blue-600" />
               Gerenciar Pagamentos e Faturas
             </span>
             <ChevronRight className="size-4.5 text-slate-400 group-hover:text-blue-600" />
-          </button>
+          </button> */}
+          <Link
+            href={`/professores/${teacher.id}/pagamentos`}
+            className="flex items-center justify-between w-full text-sm font-bold hover:text-blue-600 transition-colors group"
+          >
+            <span className="flex items-center gap-2">
+              <CreditCard className="size-4.5 text-slate-400 group-hover:text-blue-600" />
+              Gerenciar Pagamentos e Faturas
+            </span>
+
+            <ChevronRight className="size-4.5 text-slate-400 group-hover:text-blue-600" />
+          </Link>
+
         </div>
       }
     />
