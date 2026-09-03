9a2c5d0ad678e2bed187b2a8d68d5c01236f6aef
Fernanda Felix <fernandaleitefelix@gmail.com>
2026-09-03 15:57:25 -0300

    fix route

--- apps/students/lib/session.ts
+++ apps/students/lib/session.ts
@@ -48,7 +48,7 @@ export async function updateSession(request: NextRequest) {
     "/api/webhooks/pagarme",
     "/api/payments/customers",
     "/api/signup/attempts",
-    "email-preview",
+    "/email-preview",
   ];
 
   const isPublicRoute = publicRoutes.includes(pathname);
